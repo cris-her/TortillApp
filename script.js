0d02bcbadef5a6f9283978451525da1edec68b54
cris-her <crs.hdz.s@gmail.com>
2024-06-23 13:18:19 -0600

    Add support for 8 timers sets

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ document.addEventListener('DOMContentLoaded', function() {
     let timerSequences = [];
     let currentTimerIndices = [];
     let isTimerRunning = [];
-    let isFirstClick = [true, true, true];
-    let sounds = ['do-stretched.wav', 'mi-stretched.wav', 'sol-stretched.wav']
+    let isFirstClick = [true, true, true, true, true, true, true, true];
+    let sounds = ['do-stretched.wav', 're-stretched.wav', 'mi-stretched.wav', 'fa-stretched.wav', 'sol-stretched.wav', 'la-stretched.wav', 'si-stretched.wav', 'do-stretched-octave.wav']
 
     addTimerButton.addEventListener('click', function() {
         const timerCount = timerInputsContainer.querySelectorAll('.timer-input').length + 1;
@@ -34,13 +34,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
     startTimersButton.addEventListener('click', function() {
 
-        if (!isTimerRunning[1] && !isTimerRunning[2] && !isTimerRunning[3]) {
+        if (!isTimerRunning[1] && !isTimerRunning[2] && !isTimerRunning[3] && !isTimerRunning[4] && !isTimerRunning[5] && !isTimerRunning[6] && !isTimerRunning[7] && !isTimerRunning[8]) {
             const timeInputs = timerInputsContainer.querySelectorAll('.time-input');
             timerSequences[1] = Array.from(timeInputs).map(input => parseTime(input.value));
             timerSequences[2] = Array.from(timeInputs).map(input => parseTime(input.value)); // Example: Use different inputs for each set
-            timerSequences[3] = Array.from(timeInputs).map(input => parseTime(input.value)); // Example: Use different inputs for each set
+            timerSequences[3] = Array.from(timeInputs).map(input => parseTime(input.value));
+            timerSequences[4] = Array.from(timeInputs).map(input => parseTime(input.value));
+            timerSequences[5] = Array.from(timeInputs).map(input => parseTime(input.value));
+            timerSequences[6] = Array.from(timeInputs).map(input => parseTime(input.value));
+            timerSequences[7] = Array.from(timeInputs).map(input => parseTime(input.value));
+            timerSequences[8] = Array.from(timeInputs).map(input => parseTime(input.value));
 
-            if (timerSequences[1].every(timer => timer !== null) && timerSequences[2].every(timer => timer !== null)  && timerSequences[3].every(timer => timer !== null)) {
+            if (timerSequences[1].every(timer => timer !== null) && timerSequences[2].every(timer => timer !== null)  && timerSequences[3].every(timer => timer !== null) && timerSequences[4].every(timer => timer !== null) && timerSequences[5].every(timer => timer !== null) && timerSequences[6].every(timer => timer !== null) && timerSequences[7].every(timer => timer !== null) && timerSequences[8].every(timer => timer !== null)) {
                 
                 document.getElementById('config').style.display = 'none';
                 document.getElementById('timerSets').style.display = 'block';
@@ -48,23 +53,48 @@ document.addEventListener('DOMContentLoaded', function() {
                 const timerControlsContainer1 = document.getElementById('timerControls1');
                 const timerControlsContainer2 = document.getElementById('timerControls2');
                 const timerControlsContainer3 = document.getElementById('timerControls3');
+                const timerControlsContainer4 = document.getElementById('timerControls4');
+                const timerControlsContainer5 = document.getElementById('timerControls5');
+                const timerControlsContainer6 = document.getElementById('timerControls6');
+                const timerControlsContainer7 = document.getElementById('timerControls7');
+                const timerControlsContainer8 = document.getElementById('timerControls8');
 
                 timerInputsContainer.style.display = 'none';
                 timerControlsContainer1.style.display = 'flex';
                 timerControlsContainer2.style.display = 'flex';
                 timerControlsContainer3.style.display = 'flex';
+                timerControlsContainer4.style.display = 'flex';
+                timerControlsContainer5.style.display = 'flex';
+                timerControlsContainer6.style.display = 'flex';
+                timerControlsContainer7.style.display = 'flex';
+                timerControlsContainer8.style.display = 'flex';
 
                 isTimerRunning[1] = true;
                 isTimerRunning[2] = true;
                 isTimerRunning[3] = true;
+                isTimerRunning[4] = true;
+                isTimerRunning[5] = true;
+                isTimerRunning[6] = true;
+                isTimerRunning[7] = true;
+                isTimerRunning[8] = true;
 
                 currentTimerIndices[1] = 0;
                 currentTimerIndices[2] = 0;
                 currentTimerIndices[3] = 0;
+                currentTimerIndices[4] = 0;
+                currentTimerIndices[5] = 0;
+                currentTimerIndices[6] = 0;
+                currentTimerIndices[7] = 0;
+                currentTimerIndices[8] = 0;
 
                 runTimer(1);
                 runTimer(2);
                 runTimer(3);
+                runTimer(4);
+                runTimer(5);
+                runTimer(6);
+                runTimer(7);
+                runTimer(8);
 
             } else {
                 alert('Please enter valid times in the format hh:mm:ss.');
